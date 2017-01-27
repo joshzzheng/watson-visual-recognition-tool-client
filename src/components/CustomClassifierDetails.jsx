e19ffc7d4c5a4669dc2f43ef562c0235875beb24
Nick Bourdakos <bourdakos1@gmail.com>
2017-01-26 18:14:45 -0800

    update syntax and remove ajax

--- src/components/CustomClassifierDetails.jsx
+++ src/components/CustomClassifierDetails.jsx
@@ -1,133 +1,112 @@
 import React from 'react'
-import moment from 'moment'
-import $ from 'jquery'
 import request from 'superagent'
-import ClassifyImage from './ClassifyImage'
-import classNames from 'classnames'
 import ResultList from './ResultList'
 import DropButton from './DropButton'
 import Styles from './Styles'
+import Radium from 'radium'
 
-var ClassList = React.createClass({
-  render: function(){
-    var classList = this.props.classes.map(function(name){
-      return (
-        <li className="list-group-item" key={name.class}>
-          {name.class}
-        </li>
-      );
-    })
-    return (
-      <ul className="list-group list-group-flush"
-          style={{marginRight:'2em'}}>
-        {classList}
-      </ul>
-    );
-  }
-});
+@Radium
+class ClassifierDetail extends React.Component {
+    loadClassifierDetailsFromServer = () => {
+        var self = this
+        var req = request.get(this.props.host + "api/classifier" + this.props.classiferID)
 
-var CustomClassifierDetails = React.createClass({
-  getInitialState: function() {
-    return {
-      classifier: {
-        classes: []
-      }
-    };
-  },
+        req.type('json')
+        req.set('api_key', this.props.apiKey)
+        req.set('Expires', '-1')
+        req.set('Cache-Control', 'no-cache,no-store,must-revalidate,max-age=-1,private')
 
-  loadClassifierDetailsFromServer: function(){
-    $.ajax({
-      url: this.props.host + "api/classifier" + this.props.classiferID,
-      dataType: 'json',
-      cache: false,
-      data: {apiKey: this.props.apiKey},
-
-      success: function(data) {
-        this.setState({classifier: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.host, status, err.toString());
-      }.bind(this)
-    });
-  },
-
-  onDrop: function(files, onFinished) {
-      // this.props.addFile(this.props.classes, this.props.rowId, files[files.length-1])
-      var self = this
-      var req = request.post(this.props.host + "api/classify")
-
-      if (files[0]) {
-          req.attach('file', files[0])
-      }
-
-      req.field('classifier_id', this.props.classifierID)
-      req.field('api_key', this.props.apiKey)
-
-      req.on('progress', function(e) {
-          console.log(e.direction + ' Percentage done: ' + e.percent)
-      })
-
-      req.end(function(err, res) {
-          var results = res.body.images[0].classifiers[0].classes
-          results.sort(function(a, b) {
-              return b.score - a.score;
-          })
-          console.log(results)
-          self.setState({ file: files[0], results: results })
-          onFinished()
-      })
-  },
-
-  render: function() {
-    var textStyle = {
-      textDecoration:'none',
-      display:'block',
-      whiteSpace:'nowrap',
-      overflow:'hidden',
-      textOverflow:'ellipsis',
-      color: Styles.colorTextDark,
-      font: Styles.fontDefault,
+        req.end(function(err, res) {
+            self.setState({classifier: data})
+        })
     }
 
-    var titleStyle = {
-      textDecoration:'none',
-      display:'block',
-      whiteSpace:'nowrap',
-      overflow:'hidden',
-      textOverflow:'ellipsis',
-      color: Styles.colorTextDark,
-      font: Styles.fontHeader,
+    onDrop = (files, onFinished) => {
+        var self = this
+        var req = request.post(this.props.host + "api/classify")
+
+        if (files[0]) {
+            req.attach('file', files[0])
+        }
+
+        req.field('classifier_id', this.props.classifierID)
+        req.field('api_key', this.props.apiKey)
+
+        req.on('progress', function(e) {
+            console.log(e.direction + ' Percentage done: ' + e.percent)
+        })
+
+        req.end(function(err, res) {
+            var results = res.body.images[0].classifiers[0].classes
+            results.sort(function(a, b) {
+                return b.score - a.score;
+            })
+            console.log(results)
+            self.setState({ file: files[0], results: results })
+            onFinished()
+        })
     }
 
-    var cardStyle = {
-      maxWidth:'30rem',
-      marginBottom:'4rem',
-      borderRadius: '5px',
-      borderColor: '#dedede',
-      borderWidth: 'thin',
-      borderStyle: 'solid',
-      background: 'white',
-      padding: '12px',
+    render() {
+        var textStyle = {
+            paddingTop: '5px',
+            textDecoration:'none',
+            display:'block',
+            whiteSpace:'nowrap',
+            overflow:'hidden',
+            textOverflow:'ellipsis',
+            color: Styles.colorTextLight,
+            font: Styles.fontDefault,
+        }
+
+        var titleStyle = {
+            textDecoration:'none',
+            display:'block',
+            whiteSpace:'nowrap',
+            overflow:'hidden',
+            textOverflow:'ellipsis',
+            color: Styles.colorTextDark,
+            font: Styles.fontHeader,
+        }
+
+        var cardStyle = {
+            maxWidth:'30rem',
+            marginBottom:'4rem',
+            borderRadius: '5px',
+            borderColor: '#dedede',
+            borderWidth: 'thin',
+            borderStyle: 'solid',
+            background: 'white',
+            padding: '12px',
+        }
+
+        var status = {
+            marginBottom: '-1px',
+            display: 'inline-block',
+            width: '10px',
+            height: '10px',
+            borderRadius: '5px',
+            background: '#64dd17',
+        }
+
+        return(
+            <div className="col-sm-4">
+                <div style={cardStyle}>
+                    <div style={titleStyle}>{this.props.name}</div>
+                    <div style={textStyle}>{this.props.classifierID}</div>
+                    <div style={textStyle}>{this.props.status} <div style={status}/></div>
+
+                    <div style={{width: '100%', height:'20px'}}></div>
+
+                    <DropButton
+                        onDrop={this.onDrop}
+                        text={"Drag images here to classify them"}
+                        subtext={"choose your files"} />
+                    {this.state.results ? <ResultList file={this.state.file} results={this.state.results}/> : null}
+                </div>
+            </div>
+        )
     }
+}
 
-    return(
-      <div className="col-sm-4">
-        <div style={cardStyle}>
-            <div style={titleStyle}>{this.props.name}</div>
-            <div style={textStyle}><b>ID:</b> {this.props.classifierID}</div>
-            <div style={textStyle}><b>Status:</b> {this.props.status}</div>
-
-            <ClassList classes={this.state.classifier.classes} />
-
-            <DropButton
-                onDrop={this.onDrop}
-                text={"Drag images here to classify them"}
-                subtext={"choose your files"} />
-            {this.state.results ? <ResultList file={this.state.file} results={this.state.results}/> : null}
-        </div>
-      </div>
-    );
-  }
-});
-
-module.exports = CustomClassifierDetails;
+module.exports = ClassifierDetail
