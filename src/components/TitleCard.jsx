b4a785e7ab230697850ba6c01b37b0b1abc6c96c
Nick Bourdakos <bourdakos1@gmail.com>
2017-02-04 11:41:44 -0800

    remove autofill icon

--- src/components/TitleCard.jsx
+++ src/components/TitleCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Styles from './Styles'
-import Radium from 'radium'
+import Radium, {Style} from 'radium'
 
 @Radium
 export default class TitleCard extends React.Component {
@@ -26,6 +26,7 @@ export default class TitleCard extends React.Component {
             outline: 'none',
             width: '100%',
             padding: '10px',
+            paddingRight: '45px',
             ':focus': {
                 borderBottom: `1px solid ${Styles.colorPrimary}`,
             }
@@ -38,9 +39,16 @@ export default class TitleCard extends React.Component {
 
         return (
             <div style={[cardStyle, this.props.style]}>
+                <Style scopeSelector='input::-webkit-contacts-auto-fill-button' rules={{
+                    visibility: 'hidden',
+                    display: 'none !important',
+                    pointerEvents: 'none',
+                    position: 'absolute',
+                    right: '0',
+                }} />
                 {this.props.negative ?
                     <div style={[text, this.props.inputStyle]}>Negative&nbsp;&nbsp;<div style={[optional, {display: 'inline-block'}]}>(Optional)</div></div> :
-                    <input type="text" style={[text, this.props.inputStyle]}
+                    <input type='text' style={[text, this.props.inputStyle]}
                         value={this.props.default}
                         placeholder={this.props.placeholder}
                         onChange={this.props.onChange} />
