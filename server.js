1cfe4a1a55ef60c72d8777763a6c57a432ee7773
Nick Bourdakos <bourdakos1@gmail.com>
2017-02-05 00:20:22 -0800

    apparently there needs to be an image in the directory in order to function

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
-// app.use(fileUpload());
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html')
